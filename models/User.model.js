205a075334cfc6a7a280458851fcfe3e626cef64
SilviuDN <silviudilimot@gmail.com>
2023-01-23 10:39:33 +0100

    starter files

---
+++ models/User.model.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: String,
+    profileImg: { type: String, default: 'https://i.stack.imgur.com/l60Hf.png' },
+    description: { type: String, default: 'No existe descripción.' }
+    // add roles setup here
+  },
+  {
+    timestamps: true
+  }
+);
+
+
+module.exports = model('User', userSchema)
